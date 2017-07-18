dada85e6b586884aecdf7df9cd41908d0ef9a72f
termie <github@anarkystic.com>
2017-07-18 13:23:05 -0700

    add upload

--- gen_urls.py
+++ gen_urls.py
@@ -3,29 +3,36 @@ import pprint
 
 import twitter
 
+def url2name(s):
+    # url like:
+    # "https://gist.github.com/rogerallen/e13d3564f39a4102e5effd17f5334f14#file-1_archive-edn-L830-L832"
+    last = s.split("/")[-1]
+    s = s.replace("#file-1_archive-edn", "")
+    return s
+
+
 def main():
+    consumer_key=os.environ.get("TWITTER_CONSUMER_KEY")
+    consumer_secret=os.environ.get("TWITTER_CONSUMER_SECRET")
+    access_token=os.environ.get("TWITTER_ACCESS_TOKEN")
+    access_token_secret=os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
-  consumer_key=os.environ.get("TWITTER_CONSUMER_KEY")
-  consumer_secret=os.environ.get("TWITTER_CONSUMER_SECRET")
-  access_token=os.environ.get("TWITTER_ACCESS_TOKEN")
-  access_token_secret=os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
+    api = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_token,
+                      access_token_secret=access_token_secret)
 
-  api = twitter.Api(consumer_key=consumer_key,
-                    consumer_secret=consumer_secret,
-                    access_token_key=access_token,
-                    access_token_secret=access_token_secret)
+    favs = api.GetFavorites(screen_name='termie', count=200)
+    #pprint.pprint(favs)
+    urls = []
+    for fav in favs:
+        if fav.user.screen_name != 'tweegeemee':
+            continue
+        urls.append(fav.urls[0].expanded_url)
 
-  favs = api.GetFavorites(screen_name='termie')
-  #pprint.pprint(favs)
-  urls = []
-  for fav in favs:
-    if fav.user.screen_name != 'tweegeemee':
-      continue
-    urls.append(fav.urls[0].expanded_url)
-
-  for u in urls:
-    print u
+    for u in urls:
+        print u
 
 if __name__ == '__main__':
     main()
--- manual.sh
+++ manual.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+URL=$1
+echo URL=$URL
+NAME=${URL##*/}
+NAME=${NAME/\#file-1_archive-edn/}
+echo NAME=$NAME
 
 curl -H "Authorization: Bearer $WERCKER_TOKEN" \
      -H "Content-Type: application/json" \
-     -d "{\"pipelineId\": \"$PIPELINE_ID\", \"branch\": \"werckerize\", \"envVars\": [{\"key\": \"TGM_URL\", \"value\": \"$1\"}]}" \
+     -d "{\"pipelineId\": \"$PIPELINE_ID\", \"branch\": \"werckerize\", \"envVars\": [{\"key\": \"TGM_URL\", \"value\": \"$URL\"}, {\"key\": \"TGM_NAME\", \"value\": \"$NAME\"}]}" \
      https://app.wercker.com/api/v3/runs
 
 
